beaa4a17538f22694a64b1fb3feb43b08871e4b2
M4rtinYocupicio <martin.yocupicio228307@potros.itson.edu.mx>
2026-05-16 10:09:31 -0700

    Se arregló un pequeño error en la clase Product

--- app/src/main/java/mx/edu/itson/potros/foodorderapp/Product.kt
+++ app/src/main/java/mx/edu/itson/potros/foodorderapp/Product.kt
@@ -4,17 +4,41 @@ import android.os.Parcel
 import android.os.Parcelable
 
 class Product : Parcelable {
+
     var name: String? = null
     var image: Int = 0
+    var imageUrl: String? = null
     var descripcion: String? = null
     var price: Double = 0.0
     var cantidad: Int = 0
 
     constructor()
 
-    constructor(name: String, image: Int, descripcion: String, price: Double, cantidad: Int) {
+    constructor(
+        name: String,
+        image: Int,
+        descripcion: String,
+        price: Double,
+        cantidad: Int
+    ) {
         this.name = name
         this.image = image
+        this.descripcion = descripcion
+        this.price = price
+        this.cantidad = cantidad
+    }
+
+    constructor(
+        name: String,
+        image: Int,
+        imageUrl: String?,
+        descripcion: String,
+        price: Double,
+        cantidad: Int
+    ) {
+        this.name = name
+        this.image = image
+        this.imageUrl = imageUrl
         this.descripcion = descripcion
         this.price = price
         this.cantidad = cantidad
@@ -23,6 +47,7 @@ class Product : Parcelable {
     protected constructor(`in`: Parcel) {
         name = `in`.readString()
         image = `in`.readInt()
+        imageUrl = `in`.readString()
         descripcion = `in`.readString()
         price = `in`.readDouble()
         cantidad = `in`.readInt()
@@ -31,6 +56,7 @@ class Product : Parcelable {
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeString(name)
         parcel.writeInt(image)
+        parcel.writeString(imageUrl)
         parcel.writeString(descripcion)
         parcel.writeDouble(price)
         parcel.writeInt(cantidad)
@@ -40,7 +66,7 @@ class Product : Parcelable {
 
     companion object {
         @JvmField
-        val CREATOR: Parcelable.Creator<Product?> = object : Parcelable.Creator<Product?> {
+        val CREATOR = object : Parcelable.Creator<Product?> {
             override fun createFromParcel(`in`: Parcel): Product? = Product(`in`)
             override fun newArray(size: Int): Array<Product?> = arrayOfNulls(size)
         }
